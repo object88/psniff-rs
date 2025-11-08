ead6532be2b91d637289c140967b78fbd1cf2fde
object88 <object88@gmail.com>
2025-11-07 16:56:56 -0800

    chore: refactor packet handling into listener

--- src/devices.rs
+++ src/devices.rs
@@ -1,10 +1,8 @@
-use std::{collections::HashMap, net::IpAddr};
+use std::collections::HashMap;
 
 use anyhow::{Context, Result};
-use etherparse::{
-	Icmpv4Slice, Icmpv6Slice, Ipv4Slice, Ipv6Slice, NetSlice, SlicedPacket, TcpSlice, TransportSlice,
-	UdpSlice,
-};
+use etherparse::{NetSlice, SlicedPacket, TransportSlice};
+use log::{error, info};
 use pcap::{Capture, Device, Inactive};
 use tokio::sync::{broadcast::Receiver, mpsc::Sender};
 
@@ -13,28 +11,17 @@ use crate::{
 	runtime::{BlockingRunnable, BlockingRunnableBuilder},
 };
 
-// If we are going to track all traffic between particular IP addresses, the port data will need to be removed.
-// #[derive(Debug, PartialEq, Eq, Hash)]
-// struct TcpSession {
-// 	src_ip: IpAddr,
-// 	src_port: u16,
-// 	dst_ip: IpAddr,
-// 	dst_port: u16,
-// }
-
-// struct State {
-// 	seq: u32,
-// 	packet_count: u32,
-// }
-
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub enum Matcher {
+	Arp,
 	IPv4_ICMPv4,
 	IPv4_TCP,
 	IPv4_UDP,
 	IPv6_ICMPv6,
 	IPv6_TCP,
-	IPv6_UDP
+	IPv6_UDP,
+	Missing,
+	Unexpected,
 }
 
 pub struct MovingPacket {
@@ -45,14 +32,12 @@ pub struct MovingPacket {
 pub struct Builder {
 	// cfg: HttpConfig
 	iface_name: Option<String>,
-	// senders: HashMap<Matcher, Option<Sender<pcap::Packet<'static>>>>,
-	senders: HashMap<Matcher, Option<Sender<MovingPacket>>>,
+	senders: HashMap<Matcher, Sender<MovingPacket>>,
 }
 
 pub struct Devices {
 	cap: Capture<Inactive>,
-	// senders: HashMap<Matcher, Option<Sender<pcap::Packet<'static>>>>,
-	senders: HashMap<Matcher, Option<Sender<MovingPacket>>>,
+	senders: HashMap<Matcher, Sender<MovingPacket>>,
 }
 
 pub fn new<'a>(/*cfg: HttpConfig*/) -> Builder {
@@ -70,7 +55,7 @@ impl Builder {
 
 	// pub fn set_typed_sender(mut self, m: Matcher, sender: Sender<pcap::Packet<'static>>) -> Self {
 	pub fn set_typed_sender(mut self, m: Matcher, sender: Sender<MovingPacket>) -> Self {
-		self.senders.insert(m, Some(sender));
+		self.senders.insert(m, sender);
 		self
 	}
 }
@@ -110,22 +95,67 @@ impl BlockingRunnable for Devices {
 		let (mut packet_count, mut dropped_count, mut if_dropped_count) = (0, 0, 0);
 
 		loop {
+			// Check to see if we need to exit
+			if !cancel_rx.is_empty() || cancel_rx.is_closed() {
+				break;
+			}
+
 			match cap.next_packet() {
 				Ok(packet) => {
-					match SlicedPacket::from_ethernet(packet.data) {
-						Ok(value) => {
-							analyze_packet(value/*, &mut sequences*/);
-						}, // analyze_packet(value),
-						Err(err) => println!("Error parsing packet: {:?}", err),
-					}
+					let m = match SlicedPacket::from_ethernet(packet.data) {
+						Ok(sliced_packet) => {
+							match &sliced_packet.net {
+								Some(NetSlice::Arp(_)) => Matcher::Arp,
+								Some(NetSlice::Ipv4(ipv4_header)) => match &sliced_packet.transport {
+									Some(TransportSlice::Icmpv4(_)) => Matcher::IPv4_ICMPv4,
+									Some(TransportSlice::Icmpv6(_)) => Matcher::Unexpected,
+									Some(TransportSlice::Tcp(_)) => Matcher::IPv4_TCP,
+									Some(TransportSlice::Udp(_)) => Matcher::IPv4_UDP,
+									None => {
+										let ip_number = ipv4_header.payload_ip_number();
+										info!(
+											"IPv4-no-transport {} {}",
+											ip_number.keyword_str().unwrap_or("---"),
+											ip_number.protocol_str().unwrap_or("unknown")
+										);
+										continue
+									}
+								},
+								Some(NetSlice::Ipv6(ipv6_header)) => match &sliced_packet.transport {
+									Some(TransportSlice::Icmpv4(_)) => Matcher::Unexpected,
+									Some(TransportSlice::Icmpv6(_)) => Matcher::IPv6_ICMPv6,
+									Some(TransportSlice::Tcp(_)) => Matcher::IPv6_TCP,
+									Some(TransportSlice::Udp(_)) => Matcher::IPv6_UDP,
+									None => {
+										let ip_number = ipv6_header.payload().ip_number;
+										info!(
+											"IPv6-no-transport {} {}",
+											ip_number.keyword_str().unwrap_or("---"),
+											ip_number.protocol_str().unwrap_or("unknown")
+										);
+										continue
+									}
+								},
+								None => Matcher::Missing,
+							}
+						},
+						Err(err) => {
+							error!("Error parsing packet: {:?}", err);
+							continue;
+						}
+					};
+
 					let header_clone = packet.header.clone();
 					let data_clone = packet.data.to_vec();
 					let p0 = MovingPacket {
 						header: header_clone,
 						data: data_clone,
 					};
-					let s = self.senders.get(&Matcher::IPv4_TCP).unwrap().as_ref();
-					let _ = s.unwrap().blocking_send(p0);
+					let s = match self.senders.get(&m) {
+						Some(x) => x,
+						None => continue,
+					};
+					let _ = s.blocking_send(p0);
 				},
 				Err(pcap::Error::TimeoutExpired) => {
 					// Just try again on timeout - this makes the program more responsive
@@ -149,11 +179,6 @@ impl BlockingRunnable for Devices {
 					println!("Error: {}", e);
 					continue;
 				},
-			}
-
-			// Check to see if we need to exit
-			if !cancel_rx.is_empty() || cancel_rx.is_closed() {
-				break;
 			}
 		}
 
@@ -182,9 +207,6 @@ pub fn listen(cfg: ListenConfig) -> Result<()> {
 	//   Err(e) => return Err(e.into()),
 	// };
 
-	// sequences
-	// let mut sequences: HashMap<TcpSession, State> = HashMap::new();
-
 	let (mut packet_count, mut dropped_count, mut if_dropped_count) = (0, 0, 0);
 
 	loop {
@@ -192,7 +214,8 @@ pub fn listen(cfg: ListenConfig) -> Result<()> {
 			Ok(packet) => {
 				match SlicedPacket::from_ethernet(packet.data) {
 					Ok(value) => {
-						analyze_packet(value/*, &mut sequences */);
+						// analyze_packet(value/*, &mut sequences */);
+						todo!();
 					}, // analyze_packet(value),
 					Err(err) => println!("Error parsing packet: {:?}", err),
 				}
@@ -220,183 +243,6 @@ pub fn listen(cfg: ListenConfig) -> Result<()> {
 				continue;
 			},
 		}
-	}
-}
-
-fn process_ipv4_icmpv4(icmpv4_header: &Icmpv4Slice) {
-	let icmp_header = icmpv4_header.header();
-	println!("ICMPv4, type={:?}", icmp_header.icmp_type);
-}
-
-fn process_ipv4_icmpv6() {
-	// This should never happen; ICMPv6 should only ever come on IPv6
-	// ref: https://en.wikipedia.org/wiki/Internet_Control_Message_Protocol
-	// TODO: render error message
-}
-
-fn process_ipv4_tcp(
-	// sequences: &mut HashMap<TcpSession, State>,
-	_ip_header: &Ipv4Slice,
-	_tcp_header: &TcpSlice,
-) {
-	// let src_port = tcp_header.source_port();
-	// let dst_port = tcp_header.destination_port();
-	// let seq = tcp_header.sequence_number();
-
-	// let tcp_session = TcpSession {
-	// 	src_ip: IpAddr::V4(ip_header.header().source_addr()),
-	// 	src_port,
-	// 	dst_ip: IpAddr::V4(ip_header.header().destination_addr()),
-	// 	dst_port,
-	// };
-
-	// match sequences.get_mut(&tcp_session) {
-	// 	Some(last_state) => {
-	// 		// Sequence is already started
-	// 		if seq == last_state.seq {
-	// 			println!(
-	// 				"= IPv4-TCP [{}:{} -> {}:{}] SYN={} ACK={} FIN={} RST={} seq={seq}, frag={}, bytes={}, count={}",
-	// 				tcp_session.src_ip,
-	// 				tcp_session.src_port,
-	// 				tcp_session.dst_ip,
-	// 				tcp_session.dst_port,
-	// 				tcp_header.syn(),
-	// 				tcp_header.ack(),
-	// 				tcp_header.fin(),
-	// 				tcp_header.rst(),
-	// 				ip_header.is_payload_fragmented(),
-	// 				tcp_header.payload().len(),
-	// 				last_state.packet_count
-	// 			);
-	// 		} else if seq > last_state.seq {
-	// 			println!(
-	// 				"> IPv4-TCP [{}:{} -> {}:{}] SYN={} ACK={} FIN={} RST={} seq={seq}, frag={}, bytes={}, count={}",
-	// 				tcp_session.src_ip,
-	// 				tcp_session.src_port,
-	// 				tcp_session.dst_ip,
-	// 				tcp_session.dst_port,
-	// 				tcp_header.syn(),
-	// 				tcp_header.ack(),
-	// 				tcp_header.fin(),
-	// 				tcp_header.rst(),
-	// 				ip_header.is_payload_fragmented(),
-	// 				tcp_header.payload().len(),
-	// 				last_state.packet_count
-	// 			);
-	// 		} else if seq < last_state.seq {
-	// 			println!("Out of order packet!");
-	// 		}
-	// 		last_state.packet_count += 1;
-	// 		last_state.seq = seq;
-	// 	},
-	// 	None => {
-	// 		// New connection
-	// 		println!(
-	// 			"IPv4-TCP [{}:{} -> {}:{}] SYN={} ACK={} FIN={} RST={} seq={seq}, frag={}, bytes={}",
-	// 			tcp_session.src_ip,
-	// 			tcp_session.src_port,
-	// 			tcp_session.dst_ip,
-	// 			tcp_session.dst_port,
-	// 			tcp_header.syn(),
-	// 			tcp_header.ack(),
-	// 			tcp_header.fin(),
-	// 			tcp_header.rst(),
-	// 			ip_header.is_payload_fragmented(),
-	// 			tcp_header.payload().len()
-	// 		);
-	// 		let s = State {
-	// 			packet_count: 1,
-	// 			seq,
-	// 		};
-	// 		sequences.insert(tcp_session, s);
-	// 	},
-	// };
-}
-
-fn process_ipv4_udp(ip_slice: &Ipv4Slice, udp_header: &UdpSlice) {
-	let ip_header = ip_slice.header();
-	println!(
-		"IPv4-UDP [{} -> {}] [{} -> {}] bytes={}",
-		ip_header.source_addr(),
-		ip_header.destination_addr(),
-		udp_header.source_port(),
-		udp_header.destination_port(),
-		udp_header.payload().len()
-	);
-}
-
-fn process_ipv4_no_transport(/*_sequences: &mut HashMap<TcpSession, State>,*/ ip_header: &Ipv4Slice) {
-	let ip_number = ip_header.payload_ip_number();
-	println!(
-		"IPv4-no-transport {} {}",
-		ip_number.keyword_str().unwrap_or("---"),
-		ip_number.protocol_str().unwrap_or("unknown")
-	);
-	// ip_header.header().
-}
-
-fn process_ipv6_icmpv4() {
-	// This should never happen; ICMPv6 should only ever come on IPv6
-	// ref: https://en.wikipedia.org/wiki/ICMPv6
-	// TODO: render error message
-}
-
-fn process_ipv6_icmpv6(icmpv6_header: &Icmpv6Slice) {
-	let icmp_header = icmpv6_header.header();
-	println!("ICMPv6, type={:?}", icmp_header.icmp_type);
-}
-
-fn process_ipv6_tcp(
-	// _sequences: &mut HashMap<TcpSession, State>,
-	_ip_header: &Ipv6Slice,
-	tcp_header: &TcpSlice,
-) {
-	let _src_port = tcp_header.source_port();
-	let _dst_port = tcp_header.destination_port();
-	let _seq = tcp_header.sequence_number();
-	println!("IPv6-TCP");
-}
-
-fn process_ipv6_udp(ip_slice: &Ipv6Slice, udp_header: &UdpSlice) {
-	let ip_header = ip_slice.header();
-	println!(
-		"IPv6-UDP [{} -> {}] [{} -> {}] bytes={}",
-		ip_header.source_addr(),
-		ip_header.destination_addr(),
-		udp_header.source_port(),
-		udp_header.destination_port(),
-		udp_header.payload().len()
-	);
-}
-
-fn process_ipv6_no_transport(/*_sequences: &mut HashMap<TcpSession, State>,*/ ip_header: &Ipv6Slice) {
-	let ip_number = ip_header.payload().ip_number;
-	println!(
-		"IPv6-no-transport {} {}",
-		ip_number.keyword_str().unwrap_or("---"),
-		ip_number.protocol_str().unwrap_or("unknown")
-	);
-	// ip_header.header().
-}
-
-fn analyze_packet(packet: SlicedPacket /*, sequences: &mut HashMap<TcpSession, State> */) {
-	match &packet.net {
-		Some(NetSlice::Arp(_arp_header)) => { /* Do nothing */ },
-		Some(NetSlice::Ipv4(ipv4_header)) => match &packet.transport {
-			Some(TransportSlice::Icmpv4(icmpv4_header)) => process_ipv4_icmpv4(icmpv4_header),
-			Some(TransportSlice::Icmpv6(_icmpv6_header)) => process_ipv4_icmpv6(),
-			Some(TransportSlice::Tcp(tcp_header)) => process_ipv4_tcp(/*sequences,*/ ipv4_header, tcp_header),
-			Some(TransportSlice::Udp(udp_header)) => process_ipv4_udp(ipv4_header, udp_header),
-			None => process_ipv4_no_transport(/*sequences,*/ ipv4_header),
-		},
-		Some(NetSlice::Ipv6(ipv6_header)) => match &packet.transport {
-			Some(TransportSlice::Icmpv4(_icmpv4_header)) => process_ipv6_icmpv4(),
-			Some(TransportSlice::Icmpv6(icmpv6_header)) => process_ipv6_icmpv6(icmpv6_header),
-			Some(TransportSlice::Tcp(tcp_header)) => process_ipv6_tcp(/*sequences,*/ ipv6_header, tcp_header),
-			Some(TransportSlice::Udp(udp_header)) => process_ipv6_udp(ipv6_header, udp_header),
-			None => process_ipv6_no_transport(/*sequences,*/ ipv6_header),
-		},
-		None => { /* Do nothing */ },
 	}
 }
 
